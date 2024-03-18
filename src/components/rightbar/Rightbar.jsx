b50faba30c42723b71beccb8f40886efe8e6b42c
Trong Duc <trongduc22062000@gmail.com>
2024-03-18 22:33:34 +0200

    ads on right bar

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,32 +1,40 @@
 import "./rightbar.css"
 import React from 'react'
-import {Users} from '../../dummyData'
+import { Users } from '../../dummyData'
 import OnlineFriend from "../online/OnlineFriend"
+import CloseIcon from "@mui/icons-material/Close";
 
 function Rightbar() {
   return (
     <div className="rightbar">
-    <div className="rightbarWrapper">
-      <div className="birthdayContainer">
-        <img src="./assets/gift.png" alt="" className="birthdayImage" />
-        <span className="birthdayText">
-          <b>Gopal Patel</b> and <b>other friends</b> have a birthday today.
-        </span>
-      </div>
-      <img src="./assets/ad1.png" alt="" className="rightbarAdImg" />
- 
-     <hr className="rightbarHr" />
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img src="./assets/gift.png" alt="" className="birthdayImage" />
+          <span className="birthdayText">
+            <b>Gopal Patel</b> and <b>other friends</b> have a birthday today.
+          </span>
+        </div>
+        <div style={{marginTop: '10px', padding: "10px", borderRadius: "8px" }} className="ads">
+          <div style={{ display: "flex"}} >
+            <img src="./assets/ad1.png" width="50" alt="" className="rightbarAdImg" />
+            <h3 style={{ marginLeft: '10px' }}>cocacola.com</h3>
+            <CloseIcon style={{ cursor:'pointer'}} className="moreIcon"/>
+          </div>
 
-      <p className="rightbarTitle">Contacts</p>
-      <ul className="rightbarFriendList">
-        {Users.filter((user)=>{
-         return user.id >1
-        }).map((u)=>(
-          <OnlineFriend key={u.id} user={u}/>
+          <button className="adsButton">Learn more</button>
+        </div>
+        <hr className="rightbarHr" />
+
+        <p className="rightbarTitle">Contacts</p>
+        <ul className="rightbarFriendList">
+          {Users.filter((user) => {
+            return user.id > 1
+          }).map((u) => (
+            <OnlineFriend key={u.id} user={u} />
           ))}
-      </ul>
+        </ul>
+      </div>
     </div>
-  </div>
   )
 }
 
